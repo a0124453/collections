cf8510732f4ab3e41002e534cfba75531075e9e2
franklingu <franklingujunchao@gmail.com>
2015-03-20 14:10:16 +0800

    add another sql before q1 for minimize the effect of no OS caching for first run

--- PostgreSql/assign2/test.sql
+++ PostgreSql/assign2/test.sql
@@ -5,6 +5,15 @@ SET enable_bitmapscan = off; SET enable_indexscan = on;
 SET enable_indexonlyscan = off; SET enable_seqscan = off;
 EXPLAIN ANALYZE SELECT * FROM r WHERE b=9;
 SELECT pg_sleep(2);
+SELECT relname, heap_blks_read, heap_blks_hit, idx_blks_read, idx_blks_hit FROM
+pg_statio_all_tables WHERE relname = 'r';
+-- ---------------------------------Q1
+SELECT pg_stat_reset();
+SELECT dropdbbuffers('assign2');
+SET enable_bitmapscan = off; SET enable_indexscan = on;
+SET enable_indexonlyscan = off; SET enable_seqscan = off;
+EXPLAIN ANALYZE SELECT * FROM r WHERE b=9;
+SELECT pg_sleep(2);
 SELECT relname, heap_blks_read, heap_blks_hit, idx_blks_read, idx_blks_hit FROM pg_statio_all_tables WHERE relname = 'r';
 -- ---------------------------------Q1
 SELECT pg_stat_reset();
