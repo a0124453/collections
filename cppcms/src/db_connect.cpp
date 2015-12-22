29d7086292dfcc9ced371b08ab6457207536610f
franklingu <gu_jc@garena.com>
2015-12-22 17:55:02 +0800

    change function orders for evaluation

--- cppcms/src/db_connect.cpp
+++ cppcms/src/db_connect.cpp
@@ -4,27 +4,11 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
+void finish_with_error(MYSQL *con)
 {
-  MYSQL *con = mysql_init(NULL);
-
-  if (con == NULL)
-  {
-      cout << mysql_error(con) << endl;
-      exit(1);
-  }
-
-  if (mysql_real_connect(con, "localhost", "", "", NULL, 0, NULL, 0) == NULL)
-  {
-      finish_with_error(con);
-  }
-
-  execute_query(con, "use entry_db;".c_str());
-  execute_query(con, "insert into users(nickname) values(test3);".c_str());
-
-  mysql_close(con);
-
-  return 0;
+    cout << mysql_error(con) << endl;
+    mysql_close(con);
+    exit(1);
 }
 
 void execute_query(MYSQL *con, char *query)
@@ -35,9 +19,25 @@ void execute_query(MYSQL *con, char *query)
     }
 }
 
-void finish_with_error(MYSQL *con)
+int main(int argc, char **argv)
 {
-    cout << mysql_error(con) << endl;
+    MYSQL *con = mysql_init(NULL);
+
+    if (con == NULL)
+    {
+        cout << mysql_error(con) << endl;
+        exit(1);
+    }
+
+    if (mysql_real_connect(con, "localhost", "", "", NULL, 0, NULL, 0) == NULL)
+    {
+        finish_with_error(con);
+    }
+
+    execute_query(con, "use entry_db;".c_str());
+    execute_query(con, "insert into users(nickname) values(test3);".c_str());
+
     mysql_close(con);
-    exit(1);
+
+    return 0;
 }
