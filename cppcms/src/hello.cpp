a8ed1dd8e32b189ea6228f935640a35cb79e8cde
franklingu <gu_jc@garena.com>
2015-12-22 12:05:16 +0800

    change to url mapping

--- cppcms/src/hello.cpp
+++ cppcms/src/hello.cpp
@@ -2,7 +2,10 @@
 #include <cppcms/applications_pool.h>
 #include <cppcms/service.h>
 #include <cppcms/http_response.h>
+#include <cppcms/url_dispatcher.h>
+#include <cppcms/url_mapper.h>
 #include <iostream>
+#include <stdlib.h>
 #include "content.h"
 
 class hello : public cppcms::application {
@@ -10,15 +13,42 @@ public:
     hello(cppcms::service &srv) :
         cppcms::application(srv)
     {
+        dispatcher().assign("/number/(\\d+)",&hello::number,this,1);
+        mapper().assign("number","/number/{1}");
+
+        dispatcher().assign("/smile",&hello::smile,this);
+        mapper().assign("smile","/smile");
+
+        dispatcher().assign("",&hello::welcome,this);
+        mapper().assign("");
+
+        mapper().root("/hello");
     }
-    virtual void main(std::string url);
+    void number(std::string num);
+    void smile();
+    void welcome();
 };
 
-void hello::main(std::string /*url*/)
+void hello::number(std::string num)
 {
-    content::message c;
-    c.text=">>>Hello<<<";
-    render("message",c);
+    int no = atoi(num.c_str());
+    response().out() << "The number is " << no << "<br/>\n";
+    response().out() << "<a href='" << url("/") << "'>Go back</a>";
+}
+
+void hello::smile()
+{
+    response().out() << ":-) <br/>\n";
+    response().out() << "<a href='" << url("/") << "'>Go back</a>";
+}
+
+void hello::welcome()
+{
+    response().out() <<
+        "<h1> Welcome To Page with links </h1>\n"
+        "<a href='" << url("/number",1)  << "'>1</a><br>\n"
+        "<a href='" << url("/number",15) << "'>15</a><br>\n"
+        "<a href='" << url("/smile") << "' >:-)</a><br>\n";
 }
 
 int main(int argc,char ** argv)
