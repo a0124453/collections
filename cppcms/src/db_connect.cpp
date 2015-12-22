52b4f39885fadf8df25e6579ab41046783e90810
franklingu <gu_jc@garena.com>
2015-12-22 18:36:27 +0800

    added abstract functions for db management

--- cppcms/src/db_connect.cpp
+++ cppcms/src/db_connect.cpp
@@ -1,6 +1,7 @@
 #include <my_global.h>
 #include <mysql.h>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -19,25 +20,49 @@ void execute_query(MYSQL *con, char *query)
     }
 }
 
-int main(int argc, char **argv)
+MYSQL *connect_to_mysql()
 {
     MYSQL *con = mysql_init(NULL);
-
     if (con == NULL)
     {
         cout << mysql_error(con) << endl;
         exit(1);
     }
-
     if (mysql_real_connect(con, "localhost", "", "", NULL, 0, NULL, 0) == NULL)
     {
         finish_with_error(con);
     }
+    execute_query(con, "use entry_db;");
+    return con;
+}
 
-    execute_query(con, "use entry_db;".c_str());
-    execute_query(con, "insert into users(nickname) values(test3);".c_str());
+void insert_user(string nickname)
+{
+    MYSQL *con = connect_to_mysql();
+
+    execute_query(con, "insert into users(nickname) values('" + nickname + "');");
 
     mysql_close(con);
+}
+
+void update_user(string username, string nickname)
+{
+    MYSQL *con = connect_to_mysql();
+
+    execute_query(con, "update users set nickname = '" + nickname +
+        "' where username = " + username + ";");
+
+    mysql_close(con);
+}
+
+void update_profile_pic()
+{
+}
+
+int main(int argc, char **argv)
+{
+    insert_user("test insert");
+    update_user("1", "test update");
 
     return 0;
 }
